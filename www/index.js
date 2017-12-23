64f52bbc9919548b53bea5e974da0e343e57ab54
Misha Marinenko <misha@3marinenko.ml>
2017-12-23 17:19:17 +0200

    LOL

---
+++ www/index.js
@@ -0,0 +1,5 @@
+function genFuckeasy(){
+
+    document.querySelector("p").innerHTML=Math.floor(Math.random()*90000) + 10000+"-"+eval(Math.floor(Math.random()*90000) + 10000)+"-"+eval(Math.floor(1000 + Math.random() * 9000));//#async.conf/1/*
+    //#async = random
+}
